aa5aac83e0842a640165c1514ed3229c688d2c29
ChandraAwesome <120436175+ChandraAwesome@users.noreply.github.com>
2023-07-11 11:36:03 +0530

    FetchDataFromServer.cs

--- FetchDataFromServer.cs
+++ FetchDataFromServer.cs
@@ -34,67 +34,7 @@ namespace ADO.NET.CRUD
                 Read.Close();
                 connection.Close();
             }
-        }
-
-        public static void AddDataintoServer()
-        {
-            string ConnectionAstring= "Data Source=(localdb)\\MSSQLLocalDB;Initial Catalog=Assignment;Integrated Security=True;Encrypt=False;TrustServerCertificate=False;";
-
-            using (SqlConnection connection = new SqlConnection(ConnectionAstring))
-            {
-                SqlCommand command = new SqlCommand("EnterDetails", connection);
-                    command.CommandType= CommandType.StoredProcedure;
-                command.Parameters.AddWithValue("@ProductName", "Jack");
-                command.Parameters.AddWithValue("@Quantity", 7);
-                command.Parameters.AddWithValue("@Rating", 5);
-                connection.Open();
-
-                //int effectedrows = command.ExecuteNonQuery();
-
-                connection.Close();
-            }
-        }
-
-        public static void UpdateDataintoServer()
-        {
-            string connectionString = "Data Source=(localdb)\\MSSQLLocalDB;Initial Catalog=Assignment;Integrated Security=True;Encrypt=False;TrustServerCertificate=False;";
-            using (SqlConnection connection = new SqlConnection(connectionString))
-            {
-                SqlCommand command = new SqlCommand("UPDATE Details SET Name=@Name, PhoneNumber=@PhoneNumber, Address=@Address WHERE Id=@Id", connection);
-                command.Parameters.AddWithValue("@Id", 1);
-                command.Parameters.AddWithValue("@Name", "John Smith"); 
-                command.Parameters.AddWithValue("@PhoneNumber", 9876543210);
-                command.Parameters.AddWithValue("@Address", "456 Oak St");
-                connection.Open();
-                int rowsAffected = command.ExecuteNonQuery();
-                connection.Close();
-            }
-
-        }
-
-        public static void DeleteDataFromServer()
-        {
-                string connectionString = "Data Source = (localdb)\\MSSQLLocalDB; Initial Catalog = Assignment; Integrated Security = True; Encrypt = False; TrustServerCertificate = False; ";
-
-            using (SqlConnection connection = new SqlConnection(connectionString))
-            {
-                SqlCommand command = new SqlCommand("DELETE FROM Details WHERE Id = @Id", connection);
-                command.Parameters.AddWithValue("@Id",2);
-
-                connection.Open();
-                int rowsAffected = command.ExecuteNonQuery();
-                connection.Close();
-
-                if (rowsAffected > 0)
-                {
-                    Console.WriteLine("Data deleted successfully!");
-                }
-                else
-                {
-                    Console.WriteLine("No data found to delete.");
-                }
-            }
-
+        
         }
     }
 }
